26d50cd478fed63de4141f796e030f5cc332d8b8
Tim Shead <tshead@sandia.gov>
2012-03-16 22:10:33 +0000

    Test a wider range of message sizes.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@81 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- perf/run-tests.sh
+++ perf/run-tests.sh
@@ -1,18 +1,12 @@
-python run-cpp-zmq-latency.py > cpp-zmq-latency-tcp.csv
-python run-py-zmq-latency.py > py-zmq-latency-tcp.csv
-python run-cpp-phish-zmq-latency.py > cpp-phish-zmq-latency-tcp.csv
-python run-cpp-phish-mpi-latency.py > cpp-phish-mpi-latency-sm.csv
-python run-cpp-phish-mpi-latency.py --mpi-extra="--mca btl ^sm" > cpp-phish-mpi-latency-tcp.csv
+python run-cpp-zmq-latency.py --size "0/10500/500" > cpp-zmq-latency-tcp.csv
+python run-py-zmq-latency.py --size "0/10500/500" > py-zmq-latency-tcp.csv
+python run-cpp-phish-zmq-latency.py --size "0/10500/500" > cpp-phish-zmq-latency-tcp.csv
+python run-cpp-phish-mpi-latency.py --size "0/10500/500" > cpp-phish-mpi-latency-sm.csv
+python run-cpp-phish-mpi-latency.py --size "0/10500/500" --mpi-extra="--mca btl ^sm" > cpp-phish-mpi-latency-tcp.csv
 
-python run-cpp-zmq-throughput.py > cpp-zmq-throughput-tcp.csv
-python run-py-zmq-throughput.py > py-zmq-throughput-tcp.csv
-python run-cpp-phish-zmq-throughput.py > cpp-phish-zmq-throughput-tcp.csv
-#python run-cpp-phish-mpi-throughput.py > cpp-phish-mpi-throughput-sm.csv
-#python run-cpp-phish-mpi-throughput.py --mpi-extra="--mca btl ^sm" > cpp-phish-mpi-throughput-tcp.csv
+python run-cpp-zmq-throughput.py --size "0/10500/500" > cpp-zmq-throughput-tcp.csv
+python run-py-zmq-throughput.py --size "0/10500/500" > py-zmq-throughput-tcp.csv
+python run-cpp-phish-zmq-throughput.py --size "0/10500/500" > cpp-phish-zmq-throughput-tcp.csv
+#python run-cpp-phish-mpi-throughput.py --size "0/10500/500" > cpp-phish-mpi-throughput-sm.csv
+#python run-cpp-phish-mpi-throughput.py --size "0/10500/500" --mpi-extra="--mca btl ^sm" > cpp-phish-mpi-throughput-tcp.csv
 
-#python run-cpp-zmq-latency.py --address ipc://foo > cpp-zmq-latency-ipc.csv
-#python run-py-zmq-latency.py --address ipc://foo > py-zmq-latency-ipc.csv
-#python run-cpp-zmq-throughput.py --address ipc://foo > cpp-zmq-throughput-ipc.csv
-#python run-py-zmq-throughput.py --address ipc://foo > py-zmq-throughput-ipc.csv
-#python run-cpp-phish-zmq-latency.py --address ipc://foo > cpp-phish-zmq-latency-ipc.csv
-#python run-cpp-phish-zmq-throughput.py --address ipc://foo > cpp-phish-zmq-throughput-ipc.csv
