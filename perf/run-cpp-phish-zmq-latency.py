6ebe880d4c0093ad0d7bdf00bfd08adfff430be0
Tim Shead <tshead@sandia.gov>
2012-04-16 19:58:36 +0000

    Rewriting performance tests to consolidate boilerplate code.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@179 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- perf/run-cpp-phish-zmq-latency.py
+++ perf/run-cpp-phish-zmq-latency.py
@@ -1,6 +1,6 @@
 import optparse
+import perf
 import school
-import subprocess
 import sys
 
 parser = optparse.OptionParser()
@@ -9,23 +9,17 @@ parser.add_option("--size", default="0/5000/500", help="Number of bytes in each 
 (options, arguments) = parser.parse_args()
 
 size_begin, size_end, size_step = options.size.split("/")
+hosts = perf.hosts()
 
-try:
-  slurm_nodes = subprocess.check_output(["scontrol", "show", "hostnames"]).split()
-except:
-  slurm_nodes = []
-a_host = slurm_nodes[0] if len(slurm_nodes) == 2 else "localhost"
-b_host = slurm_nodes[1] if len(slurm_nodes) == 2 else "localhost"
-
-sys.stderr.write("Testing C++ / Phish / ZMQ latency on nodes %s and %s ...\n" % (a_host, b_host))
+sys.stderr.write("Testing C++ / Phish / ZMQ latency on %s and %s ...\n" % (hosts[0], hosts[1]))
 sys.stderr.flush()
 sys.stdout.write("cpp-phish-zmq elapsed [s],cpp-phish-zmq message size [B],cpp-phish-zmq message count,cpp-phish-zmq latency [us]\n")
 sys.stdout.flush()
 for size in range(int(size_begin), int(size_end), int(size_step)):
   sys.stderr.write("message size: %s\n" % (size))
   sys.stderr.flush()
-  a = school.add_minnows("a", [a_host], ["${CMAKE_CURRENT_BINARY_DIR}/phish-zmq-latency-a", str(size), str(options.count)])
-  b = school.add_minnows("b", [b_host], ["${CMAKE_CURRENT_BINARY_DIR}/phish-zmq-latency-b", str(size), str(options.count)])
+  a = school.add_minnows("a", [hosts[0]], ["${CMAKE_CURRENT_BINARY_DIR}/phish-zmq-latency-a", str(size), str(options.count)])
+  b = school.add_minnows("b", [hosts[1]], ["${CMAKE_CURRENT_BINARY_DIR}/phish-zmq-latency-b", str(size), str(options.count)])
   school.one_to_one(a, 0, 0, b)
   school.one_to_one(b, 0, 0, a)
   school.start()
