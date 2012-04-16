5dd1300950a1c6ba51a5b5aad29561ee3e48444e
Tim Shead <tshead@sandia.gov>
2012-04-16 19:50:33 +0000

    Move some performance-testing boilerplate into its own Python module.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@178 76edfe0a-4bdc-4aa6-b265-a765f6f55952

---
+++ perf/perf.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def hosts():
+  try:
+    slurm_nodes = subprocess.check_output(["scontrol", "show", "hostnames"]).split()
+    return [slurm_nodes[0], slurm_nodes[1]]
+  except:
+    return ["localhost", "localhost"]
+
+def arguments(a_arguments, b_arguments):
+  temp_hosts = hosts()
+  if temp_hosts[0] != "localhost" and temp_hosts[0] != "127.0.0.1":
+    a_arguments = ["ssh", "-x", temp_hosts[0]] + a_arguments
+  if temp_hosts[1] != "localhost" and temp_hosts[1] != "127.0.0.1":
+    b_arguments = ["ssh", "-x", temp_hosts[1]] + b_arguments
+  return a_arguments, b_arguments
