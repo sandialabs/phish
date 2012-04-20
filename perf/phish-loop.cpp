81ac4da47a89bda4905f0ad47b2be15702917bf6
Tim Shead <tshead@sandia.gov>
2012-04-20 22:52:50 +0000

    Switch phish-loop to the C++ binding.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@208 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- perf/phish-loop.cpp
+++ perf/phish-loop.cpp
@@ -1,68 +1,65 @@
-// MINNOW loop
-// send messages around a ring
-
-#include "phish.h"
+#include <phish.hpp>
 #include <iostream>
 
 int n,count,tail;
 char *buf;
 
-void loop(int);
+void loop(int nvalues)
+{
+  if(!tail)
+  {
+    phish::repack();
+    phish::send(0);
+    return;
+  }
+
+  if(++count < n)
+    return;
+
+  phish::close(0);
+}
 
 /* ---------------------------------------------------------------------- */
 
-int main(int narg, char **args)
+int main(int argc, char **argv)
 {
-  phish_init(&narg,&args);
-  phish_input(0,loop,NULL,1);
-  phish_output(0);
-  phish_check();
+  phish::init(argc, argv);
+  phish::input(0,loop,NULL,1);
+  phish::output(0);
+  phish::check();
 
-  if (narg != 3) phish_error("Loop syntax: loop N M");
-  n = atoi(args[1]);
-  int m = atoi(args[2]);
+  if (argc != 3) phish::error("Loop syntax: loop N M");
+  n = atoi(argv[1]);
+  int m = atoi(argv[2]);
 
-  if (n == 0) phish_error("N > 0 is required");
+  if (n == 0) phish::error("N > 0 is required");
 
   const std::string message(m, '*');
   count = 0;
 
-  int idlocal = phish_query("idlocal",0,0);
-  int nlocal = phish_query("nlocal",0,0);
+  int idlocal = phish::query("idlocal",0,0);
+  int nlocal = phish::query("nlocal",0,0);
   int head = 0;
   tail = 0;
   if (idlocal == 0) head = 1;
   if (idlocal == nlocal-1) tail = 1;
 
   if (head) {
-    double time_start = phish_timer();
+    double time_start = phish::timer();
     for (int i = 0; i < n; i++) {
-      phish_pack_raw(const_cast<char*>(message.data()),message.size());
-      phish_send(0);
+      phish::pack(message);
+      phish::send(0);
     }
-    phish_loop();
-    const double elapsed = phish_timer() - time_start;
+    phish::loop();
+    const double elapsed = phish::timer() - time_start;
     const double throughput = n / elapsed;
     const double megabits = (throughput * m * 8.0) / 1000000.0;
 
     std::cout << elapsed << "," << m << "," << n << "," << throughput << "," << megabits << "," << nlocal << "\n";
 
-  } else phish_loop();
+  } else phish::loop();
 
   delete [] buf;
-  phish_exit();
+  phish::exit();
 }
 
-/* ---------------------------------------------------------------------- */
-
-void loop(int nvalues)
-{
-  if (!tail) {
-    phish_repack();
-    phish_send(0);
-    return;
-  }
-  count++;
-  if (count < n) return;
-  phish_close(0);
-}
