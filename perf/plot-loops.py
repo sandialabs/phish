501614c38a18765c7e2e7c193f40081b97b184ea
Tim Shead <tshead@sandia.gov>
2012-04-30 18:23:36 +0000

    More work on loop-test plotting.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@216 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- perf/plot-loops.py
+++ perf/plot-loops.py
@@ -12,11 +12,15 @@ def plot_loops(path, label, color):
   rows = [row for row in reader]
   columns = zip(*rows)
 
-  x = [float(value) for value in columns[5][1:]]
-  y = [float(value) for value in columns[3][1:]]
-  s = [math.pow(float(value) / 1000, 2) + 20 for value in columns[1][1:]]
+  message_size = columns[1][1:]
+  loop_size = columns[5][1:]
+  rate = columns[3][1:]
 
-  matplotlib.pyplot.scatter(x, y, s=s, label=label, color=color)
+  for size in set(message_size):
+    x = [float(loop_size[i]) for i in range(len(loop_size)) if message_size[i] == size]
+    y = [float(rate[i]) for i in range(len(rate)) if message_size[i] == size]
+    matplotlib.pyplot.plot(x, y, label="%s byte %s" % (size, label), color=color, linewidth=float(size) / 5000 + 1)
+
   matplotlib.pyplot.xlabel("Loop size (minnows)")
   matplotlib.pyplot.ylabel("Rate (messages/S)")
 
@@ -25,6 +29,6 @@ plot_loops("cpp-phish-zmq-loop-tcp.csv", "C++ / Phish / ZMQ", "red")
 plot_loops("cpp-phish-mpi-loop-tcp.csv", "C++ / Phish / MPI / TCP", "green")
 plot_loops("cpp-phish-mpi-loop-sm.csv", "C++ / Phish / MPI / Fastest", "blue")
 matplotlib.pyplot.legend(loc="upper right")
-matplotlib.pyplot.ylim(ymin=0)
+#matplotlib.pyplot.yscale("log")
 
 matplotlib.pyplot.show()
