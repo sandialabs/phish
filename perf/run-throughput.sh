c20c7164fb06e74eec3c5837579b1b1e86e9481e
Tim Shead <tshead@sandia.gov>
2012-05-04 18:01:00 +0000

    Update test scripts.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@233 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- perf/run-throughput.sh
+++ perf/run-throughput.sh
@@ -1,7 +1,7 @@
-#python run-cpp-zmq-throughput.py > cpp-zmq-throughput-tcp.csv
+python run-cpp-zmq-throughput.py > cpp-zmq-throughput-tcp.csv
 python run-cpp-mpi-throughput.py > cpp-mpi-throughput-fastest.csv
-#python run-cpp-phish-zmq-throughput.py > cpp-phish-zmq-throughput-tcp.csv
-#python run-cpp-phish-mpi-throughput.py > cpp-phish-mpi-throughput-fastest.csv
+python run-cpp-phish-zmq-throughput.py > cpp-phish-zmq-throughput-tcp.csv
+python run-cpp-phish-mpi-throughput.py > cpp-phish-mpi-throughput-fastest.csv
 
 
 ##python run-cpp-phish-mpi-throughput.py --mpi-extra="--mca btl self,tcp" > cpp-phish-mpi-throughput-tcp.csv
