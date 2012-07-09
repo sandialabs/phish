aecb24fbfd69a90ca343ce92ca6d8655c497e2b1
Tim Shead <tshead@sandia.gov>
2012-07-09 15:20:24 +0000

    Consolidate all of our launch-related functionality into bait.py.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@270 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- bait/bait.py
+++ bait/bait.py
@@ -1,451 +1,139 @@
-#!/usr/local/bin/python
+import phish.bait
+import optparse
+import os
+import re
+import sys
 
-# ----------------------------------------------------------------------
-#   PHISH = Parallel Harness for Informatic Stream Hashing
-#   http://www.sandia.gov/~sjplimp/phish.html
-#   Steve Plimpton, sjplimp@sandia.gov, Sandia National Laboratories
-#
-#   Copyright (2012) Sandia Corporation.  Under the terms of Contract
-#   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-#   certain rights in this software.  This software is distributed under 
-#   the modified Berkeley Software Distribution (BSD) License.
-#
-#   See the README file in the top-level PHISH directory.
-# -------------------------------------------------------------------------
+parser = optparse.OptionParser()
+parser.add_option("--graphviz", default=False, action="store_true", help="Use the graphviz backend.")
+parser.add_option("--mpi-config", default=False, action="store_true", help="Use the MPI config file backend.")
+parser.add_option("--path", default="", help="Specify a colon-delimited list of paths to be prepended to executable names.")
+parser.add_option("--set", "-s", action="append", nargs=2, default=[], help="Set a backend-specific name-value pair.")
+parser.add_option("--suffix", default="", help="Add a common suffix to minnow executables.")
+parser.add_option("--variable", "-v", action="append", nargs=2, default=[], help="Specify a variable value.")
+parser.add_option("--verbose", default=False, action="store_true", help="Verbose output.  Default: %default.")
+parser.add_option("--zmq", default=False, action="store_true", help="Use the ZMQ backend.")
+options, arguments = parser.parse_args()
 
-# bait.py tool for PHISH
-# converts a PHISH input script to a MPI or socket launch script
+paths = options.path.split(":")
+settings = dict(options.set)
+variables = dict([(key, [value]) for key, value in options.variable])
 
-import sys,re,os
-
-# print error message and quit
-
-def error(msg):
-  print "ERROR:",msg
-  sys.exit()
-
-# extract next command from lines of input script
-# return command name and args, delete processed lines from lines
-# return "" at end of input script
-# allow for blank lines, comment char "#", continuation char "&"
-# perform variable replacement
-
-def next_command(lines):
-  if not lines: return "",[]
-  line = ""
-  while not line and lines: 
-    line = lines.pop(0).strip()
-    index = line.find('#')
-    if index >= 0: line = line[:index].rstrip()
-  if not line: return "",[]
-
-  while line[-1] == '&':
-    line = line[:-1]
-    if not lines: error("Command %s is incomplete" % line)
-    addline = lines.pop(0).strip()
-    if not addline: error("Command %s is incomplete" % line)
-    index = addline.find('#')
-    if index >= 0: addline = addline[:index].rstrip()
-    if not addline: error("Command %s is incomplete" % line)
-    line += addline
-    print line
-
-  pattern = "(\$[^{])"
-  matches = re.findall(pattern,line)
-  pattern = "(\$\{.+?\})"
-  matches += re.findall(pattern,line)
-  for match in matches:
-    if len(match) == 2: vname = match[1]
-    else: vname = match[2:-1]
-    if vname not in variables:
-      error("Variable %s does not exist" % vname)
-    index = line.index(match)
-    line = line[:index] + " ".join(variables[vname][1]) + \
-        line[index+len(match):]
-
-  words = line.split()
-  return words[0],words[1:]
-  
-# set a global setting via command in input script
-
-def set(args):
-  global memchunk,safe,port
-  if args[0] == "memory":
-    if len(args) != 2: error("Illegal set command");
-    memchunk = int(args[1])
-    if memchunk < 0: error("Illegal set command");
-  elif args[0] == "safe":
-    if len(args) != 2: error("Illegal set command");
-    safe = int(args[1])
-    if safe < 0: error("Illegal set command");
-  else: error("Unrecognized set parameter %s" % arg[0])
-
-# convert non-default global settings to a minnow param string
-
-def set2param():
-  str = ""
-  if memchunk != MEMCHUNK: str += " -memory %d" % memchunk
-  if safe != SAFE: str += " -safe %d" % safe
-  return str
-
-# create a variable via variable command in input script
-# these variables are flagged with a 1, command-line vars with 0
-
-def variable(args):
-  if len(args) < 2: error("Illegal variable command");
-  if args[0] in variables:
-    if variables[args[0]][0] == 0: return
-    else: error("Variable %s already in use" % args[0]);
-  variables[args[0]] = (1,args[1:])
-
-# MINNOW class instantiated by minnow command in input script
-
-class minnow:
-  def __init__(self,args):
-    narg = len(args)
-    if narg < 2: error("Invalid minnow command")
-    self.id = args[0]
-    self.exe = args[1]
-    self.args = args[2:]
-    self.send = []
-    self.recv = []
-    ids = [minnow.id for minnow in minnows]
-    if self.id in ids: error("Minnow ID %s already defined" % self.id)
-
-# HOOK class instantiated by hook command in input script
-# special case handling of first/last arg for publish/subscribe styles
-    
-class hook:
-  def __init__(self,args):
-    narg = len(args)
-    if narg < 3: error("Invalid hook command")
-    self.sender = args[0]
-    if ':' in self.sender and args[1] != "subscribe":
-      self.sender,self.sendport = self.sender.split(':',1)
-      self.sendport = int(self.sendport)
-    else: self.sendport = 0
-    self.style = args[1]
-    self.receiver = args[2]
-    if ':' in self.receiver:
-      self.receiver,self.recvport = self.receiver.split(':',1)
-      self.recvport = int(self.recvport)
-    else: self.recvport = 0
-
-    if self.style == "publish":
-      self.socket = self.receiver
-      self.receiver = ""
-      self.recvport = -1
-    if self.style == "subscribe":
-      self.socket = self.sender
-      self.sender = ""
-      self.sendport = -1
-      
-    ids = [minnow.id for minnow in minnows]
-    if self.sender and self.sender not in ids:
-      error("Unrecognized hook ID %s" % self.sender)
-    if self.receiver and self.receiver not in ids:
-      error("Unrecognized hook ID %s" % self.receiver)
-    
-# SCHOOL class instantiated by school command in input script
-
-class school:
-  def __init__(self,args):
-    narg = len(args)
-    if narg < 2: error("Invalid school command")
-    self.id = args[0]
-    self.nprocs = int(args[1])
-    ids = [school.id for school in schools]
-    if self.id in ids: error("School ID %s already defined" % self.id)
-    
-    self.invoke = None
-    self.host = None
-    iarg = 2
-    while iarg < narg:
-      if args[iarg] == "invoke":
-        if iarg+2 > narg: error("Invalid school command")
-        self.invoke = args[iarg+1]
-        iarg += 2
-      elif args[iarg] == "host":
-        if iarg+2 > narg: error("Invalid school command")
-        self.host = args[iarg+1]
-        iarg += 2
-      else: error("Invalid school command")
-        
-# mode-dependent output
-# sendport and recvport are strings which can encode more info than an int
-
-def output_mpich():
-  fp = open(outfile,"w")
-  
-  for iminnow,minnow in enumerate(minnows):
-    procstr = "-n %d" % (minnow.nprocs)
-    
-    if minnow.invoke: exestr = " %s %s" % (minnow.invoke,minnow.pathexe)
-    else: exestr = " %s" % minnow.pathexe
-    
-    minnowstr = " -minnow %s %s %d %d" % \
-        (minnow.exe,minnow.id,minnow.nprocs,minnow.procstart)
-
-    paramstr = set2param()
-    
-    instr = ""
-    for recv in minnow.recv:
-      if recv[0] >= 0:
-        nprocs_send = minnows[recv[0]].nprocs
-        procstart_send = minnows[recv[0]].procstart
-      else: nprocs_send = procstart_send = -1
-      if recv[2] >= 0:
-        nprocs_recv = minnows[recv[2]].nprocs
-        procstart_recv = minnows[recv[2]].procstart
-      else: nprocs_recv = procstart_recv = -1
-      style = hooks[recv[1]].style
-      if style == "subscribe": style += "/" + hooks[recv[1]].socket
-      instr += " -in %d %d %d %s %d %d %d" % \
-          (nprocs_send,procstart_send,hooks[recv[1]].sendport,
-           style,
-           nprocs_recv,procstart_recv,hooks[recv[1]].recvport)
-          
-    outstr = ""
-    for send in minnow.send:
-      if send[0] >= 0:
-        nprocs_send = minnows[send[0]].nprocs
-        procstart_send = minnows[send[0]].procstart
-      else: nprocs_send = procstart_send = -1
-      if send[2] >= 0:
-        nprocs_recv = minnows[send[2]].nprocs
-        procstart_recv = minnows[send[2]].procstart
-      else: nprocs_recv = procstart_recv = -1
-      style = hooks[send[1]].style
-      if style == "publish": style += "/" + hooks[send[1]].socket
-      outstr += " -out %d %d %d %s %d %d %d" % \
-          (nprocs_send,procstart_send,hooks[send[1]].sendport,
-           style,
-           nprocs_recv,procstart_recv,hooks[send[1]].recvport)
-
-    launchstr = procstr + exestr + minnowstr + paramstr + instr + outstr
-    if minnow.args: launchstr += " -args " + " ".join(minnow.args)
-    # should just need following line, but MPICH has a configfile bug
-    # print >>fp,launchstr
-    print >>fp,launchstr,
-    if iminnow < len(minnows)-1: print >>fp,":"
-    else: print >>fp
-  fp.close()
-
-def output_openmpi():
-  fp = open("outfile","w")
-  print >>fp,"mpirun",
-
-  for iminnow,minnow in enumerate(minnows):
-    procstr = "-n %d %s" % (minnow.nprocs,minnow.pathexe)
-    
-    minnowstr = " -minnow %s %s %d %d" % \
-        (minnow.exe,minnow.id,minnow.nprocs,minnow.procstart)
-
-    paramstr = set2param()
-
-    instr = ""
-    for recv in minnow.recv:
-      if recv[0] >= 0:
-        nprocs_send = minnows[recv[0]].nprocs
-        procstart_send = minnows[recv[0]].procstart,
-      else: nprocs_send = procstart_send = -1
-      if recv[2] >= 0:
-        nprocs_recv = minnows[recv[2]].nprocs
-        procstart_recv = minnows[recv[2]].procstart,
-      else: nprocs_recv = procstart_recv = -1
-      style = hooks[recv[1]].style
-      if style == "subscribe": style += "/" + hooks[recv[1]].socket
-      instr += " -in %d %d %d %s %d %d %d" % \
-          (nprocs_send,procstart_send,hooks[recv[1]].sendport,
-           style,
-           nprocs_recv,procstart_recv,hooks[recv[1]].recvport)
-
-    outstr = ""
-    for send in minnow.send:
-      if send[0] >= 0:
-        nprocs_send = minnows[send[0]].nprocs
-        procstart_send = minnows[send[0]].procstart,
-      else: nprocs_send = procstart_send = -1
-      if send[2] >= 0:
-        nprocs_recv = minnows[send[2]].nprocs
-        procstart_recv = minnows[send[2]].procstart,
-      else: nprocs_recv = procstart_recv = -1
-      style = hooks[send[1]].style
-      if style == "publish": style += "/" + hooks[send[1]].socket
-      outstr += " -out %d %d %d %s %d %d %d" % \
-          (nprocs_send,procstart_send,hooks[send[1]].sendport,
-           style,
-           nprocs_recv,procstart_recv,hooks[send[1]].recvport)
-          
-    launchstr = procstr + minnowstr + paramstr + instr + outstr
-    if minnow.args: launchstr += " -args " + " ".join(minnow.args)
-    print >>fp,launchstr,
-    if iminnow < len(minnows)-1: print >>fp,":",
-    else: print >>fp
-  fp.close()
-
-def output_socket():
-  return
-
-# ---------------------------------------------------------------------------
-# MAIN program
-
-from version import version
-print "PHISH (%s)" % version
-
-# parse command-line args to override default settings
-# command-line variables are flagged with a 0, input script vars with 1
-
-narg = len(sys.argv)
-args = sys.argv
-
-variables = {}
-outfile = "outfile"
-paths = [""]
-mode = "mpich"
-
-iarg = 1
-while iarg < narg:
-  if args[iarg] == "-var" or args[iarg] == "-v":
-    if iarg+3 > narg: error("Invalid command line args")
-    start = stop = iarg+2
-    while stop < narg and args[stop][0] != '-': stop += 1
-    variables[args[iarg+1]] = (0,args[start:stop])
-    iarg += 2+stop-start
-  elif args[iarg] == "-out" or args[iarg] == "-o":
-    if iarg+2 > narg: error("Invalid command line args")
-    outfile = args[iarg+1]
-    iarg += 2
-  elif args[iarg] == "-path" or args[iarg] == "-p":
-    if iarg+2 > narg: error("Invalid command line args")
-    paths += args[iarg+1].split(':')
-    iarg += 2
-  elif args[iarg] == "-mode" or args[iarg] == "-m":
-    if iarg+2 > narg: error("Invalid command line args")
-    mode = args[iarg+1]
-    if mode != "mpich" and mode != "openmpi" and mode != "socket":
-      error("Invalid command line args");
-    iarg += 2
-  else: error("Invalid command line args")
-
-if mode == "socket": error("Socket mode not yet supported")
-
-# defaults for variables specfied by set command
-
-memchunk = MEMCHUNK = 1
-safe = SAFE = 0
-
-# initialize data structures for minnows, hooks, schools
-
-minnows = []
+schools = {}
 hooks = []
-schools = []
 
-# read and process the input script
+# Parse the input script ...
+if len(arguments) == 1:
+  script = open(arguments[0], "r")
+else:
+  script = sys.stdin
 
-lines = sys.stdin.readlines()
-while lines:
-  command,args = next_command(lines)
-  if not command: break
-  elif command == "set": set(args)
-  elif command == "variable": variable(args)
-  elif command == "minnow": minnows.append(minnow(args))
-  elif command == "hook": hooks.append(hook(args))
-  elif command == "school": schools.append(school(args))
-  else: error("Unrecognized command %s" % command)
+for line_number, line in enumerate(script):
 
-# add fields to each minnow based on school and hook params
-# check that hooks are consistent with school
-  
-minnowids = [minnow.id for minnow in minnows]
-schoolids = [school.id for school in schools]
+  # Skip empty lines ...
+  line = line.strip()
+  if len(line) == 0:
+    continue
 
-nprocs = 0
-for minnow in minnows:
-  if minnow.id not in schoolids: minnow.nprocs = 1
+  # Skip commented lines ...
+  if line[:1] == "#":
+    continue
+
+  # Split the line into a command and a sequence of arguments ...
+  arguments = line.split()
+  command = arguments[0]
+  arguments = arguments[1:]
+
+  # Do variable expansion on each argument ...
+  expanded = []
+  for argument in arguments:
+    match = re.match("\${?([^}]*)}?", argument)
+    if match:
+      name = match.group(1)
+      if name not in variables:
+        raise Exception("Unknown variable '%s'" % name)
+      expanded += variables[name]
+    else:
+      expanded.append(argument)
+  arguments = expanded
+
+  if command == "set":
+    key = arguments[0]
+    value = arguments[1]
+    settings[key] = value
+
+  elif command == "variable":
+    key = arguments[0]
+    values = arguments[1:]
+    if key not in variables:
+      variables[key] = values
+
+  elif command == "minnow":
+    id = arguments[0]
+    arguments = arguments[1:]
+    schools[id] = {"arguments" : arguments, "count" : 1, "host" : ""}
+
+  elif command == "hook":
+    output = arguments[0].split(":")
+    style = arguments[1]
+    input = arguments[2].split(":")
+    hooks.append((output[0], int(output[1]) if len(output) > 1 else 0, style, int(input[1]) if len(input) > 1 else 0, input[0]))
+
+  elif command == "school":
+    id = arguments[0]
+    count = arguments[1]
+    keywords = arguments[2:]
+    schools[id]["count"] = int(count)
+    for key, value in keywords:
+      if key == "host":
+        schools[id]["host"] = value
+      elif key == "invoke":
+        schools[id]["arguments"] = [value] + schools[id]["arguments"]
+
   else:
-    index = schoolids.index(minnow.id)
-    minnow.nprocs = schools[index].nprocs
-    minnow.invoke = schools[index].invoke
-  minnow.procstart = nprocs
-  nprocs += minnow.nprocs
-  
-for ihook,hook in enumerate(hooks):
-  if hook.sender: sendindex = minnowids.index(hook.sender)
-  else: sendindex = -1
-  if hook.receiver: recvindex = minnowids.index(hook.receiver)
-  else: recvindex = -1
+    raise Exception("Unknown command '%s' on line %s: %s" % (command, line_number, line))
 
-  if sendindex >= 0:
-    minnows[sendindex].send.append([sendindex,ihook,recvindex])
-  if recvindex >= 0:
-    minnows[recvindex].recv.append([sendindex,ihook,recvindex])
+# Add suffixes to school executables ...
+for school in schools.values():
+  executable = school["arguments"][0]
+  executable = executable + options.suffix
+  school["arguments"][0] = executable
 
-  if sendindex >= 0:
-    npsend = minnows[sendindex].nprocs
-    sid = minnows[sendindex].id
-  if recvindex >= 0:
-    nprecv = minnows[recvindex].nprocs
-    rid = minnows[recvindex].id
-  
-  if hook.style == "single":
-    if nprecv != 1:
-      error("Invalid hook between %s and %s" % (sid,rid));
-  elif hook.style == "paired":
-    if npsend != nprecv:
-      error("Invalid hook between %s and %s" % (sid,rid));
-  elif hook.style == "hashed":
-    continue
-  elif hook.style == "roundrobin":
-    continue
-  elif hook.style == "direct":
-    continue
-  elif hook.style == "bcast":
-    continue
-  elif hook.style == "chain":
-    if sid != rid or npsend == 1:
-      error("Invalid hook between %s and %s" % (sid,rid));
-  elif hook.style == "ring":
-    if sid != rid or npsend == 1:
-      error("Invalid hook between %s and %s" % (sid,rid));
-  elif hook.style == "publish":
-    continue
-  elif hook.style == "subscribe":
-    continue
-  else:
-    error("Unrecognized hook style %s" % hook.style);
-
-# generate full executable names using list of paths
-
-for minnow in minnows:
-  flag = 0
+# Locate school executables ...
+for school in schools.values():
+  executable = school["arguments"][0]
   for path in paths:
-    if not path: pathexe = minnow.exe
-    else: pathexe = path + '/' + minnow.exe
-    if os.path.isfile(pathexe):
-      if not os.access(pathexe,os.X_OK):
-        error("Minnow %s is not executable" % pathexe)
-      minnow.pathexe = pathexe
-      flag = 1
-      break
-  if not flag: error("Minnow %s could not be found in path list" % minnow.exe)
+    if os.access(os.path.join(path, executable), os.X_OK):
+      executable = os.path.join(path, executable)
+  school["arguments"][0] = executable
 
-# create output depending on mode
+# Optionally display all of the school command lines ...
+if options.verbose:
+  for name, value in settings.items():
+    sys.stderr.write("Launch setting: %s %s\n" % (name, value))
 
-if mode == "mpich": output_mpich()
-elif mode == "openmpi": output_openmpi()
-elif mode == "socket": output_socket()
+  for school in schools.values():
+    sys.stderr.write("Launch school:  %s\n" % (" ".join(school["arguments"])))
+    sys.stderr.flush()
 
-# print stats
+# Pass the parsed data to the bait backend ...
+if options.graphviz + options.mpi_config + options.zmq != 1:
+  raise Exception("You must specify a single backend using --graphviz, --mpi-config, or --zmq.")
 
-print "# of minnows =",len(minnows)
-print "# of processes =",nprocs
+if options.graphviz:
+  phish.bait.backend("graphviz")
+if options.mpi_config:
+  phish.bait.backend("mpi-config")
+if options.zmq:
+  phish.bait.backend("zmq")
 
-# tell user for how to invoke launch script
+for name, value in settings.items():
+  phish.bait.set(name, value)
 
-if mode == "mpich":
-  print "MPICH: mpiexec -configfile %s" % outfile
-elif mode == "openmpi":
-  print "OpenMPI: invoke %s from shell" % outfile
-elif mode == "socket":
-  print "Sockets: invoke %s from shell" % outfile
+for id, school in schools.items():
+  phish.bait.school(id, [school["host"]] * school["count"], school["arguments"])
+
+for output_id, output_port, style, input_port, input_id in hooks:
+  phish.bait.hook(output_id, output_port, style, input_port, input_id)
+
+phish.bait.start()
