bb515ba9a03f11cc8a2d4e8a963155de347080e4
Tim Shead <tshead@sandia.gov>
2012-07-24 19:14:05 +0000

    The hook-styles.py example contained out-of-date API.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@299 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- example/hook-styles.py
+++ example/hook-styles.py
@@ -6,33 +6,33 @@
 # replace "import bait.graphviz" with "import bait.mpi" or "import bait.zmq" to
 # do the actual computation.
 
-import bait.graphviz
+import phish.bait.graphviz
 
-bait.minnows("a", ["localhost"] * 1, []);
+phish.bait.school("a", ["localhost"] * 1, []);
 
-bait.minnows("b", ["localhost"] * 3, []);
-bait.hook("a", 0, bait.DIRECT, 0, "b");
+phish.bait.school("b", ["localhost"] * 3, []);
+phish.bait.hook("a", 0, phish.bait.DIRECT, 0, "b");
 
-bait.minnows("c", ["localhost"] * 1, []);
-bait.hook("b", 0, bait.SINGLE, 0, "c");
+phish.bait.school("c", ["localhost"] * 1, []);
+phish.bait.hook("b", 0, phish.bait.SINGLE, 0, "c");
 
-bait.minnows("d", ["localhost"] * 3, []);
-bait.hook("c", 0, bait.ROUND_ROBIN, 0, "d");
+phish.bait.school("d", ["localhost"] * 3, []);
+phish.bait.hook("c", 0, phish.bait.ROUND_ROBIN, 0, "d");
 
-bait.minnows("e", ["localhost"] * 3, []);
-bait.hook("d", 0, bait.PAIRED, 0, "e");
+phish.bait.school("e", ["localhost"] * 3, []);
+phish.bait.hook("d", 0, phish.bait.PAIRED, 0, "e");
 
-bait.minnows("f", ["localhost"] * 3, []);
-bait.hook("e", 0, bait.HASHED, 0, "f");
+phish.bait.school("f", ["localhost"] * 3, []);
+phish.bait.hook("e", 0, phish.bait.HASHED, 0, "f");
 
-bait.minnows("g", ["localhost"] * 3, []);
-bait.hook("f", 0, bait.BROADCAST, 0, "g");
+phish.bait.school("g", ["localhost"] * 3, []);
+phish.bait.hook("f", 0, phish.bait.BROADCAST, 0, "g");
 
-bait.minnows("h", ["localhost"] * 7, []);
-bait.hook("h", 0, bait.CHAIN, 0, "h");
+phish.bait.school("h", ["localhost"] * 7, []);
+phish.bait.hook("h", 0, phish.bait.CHAIN, 0, "h");
 
-bait.minnows("i", ["localhost"] * 7, []);
-bait.hook("i", 0, bait.RING, 0, "i");
+phish.bait.school("i", ["localhost"] * 7, []);
+phish.bait.hook("i", 0, phish.bait.RING, 0, "i");
 
-bait.start();
+phish.bait.start();
 
