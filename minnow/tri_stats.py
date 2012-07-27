4eff757718b15ce0c8bf858fe1dfacff8e281ba7
Steve Plimpton <sjplimp@sandia.gov>
2012-07-27 23:27:34 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@350 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- minnow/tri_stats.py
+++ minnow/tri_stats.py
@@ -4,17 +4,20 @@ import phish
 # print vertices of tri in order such that vi < vj < vk
 
 def count(nvalues):
-  global n
-  n += 1
+  global n,ndup
+  ndup += 1
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
   type,vk,tmp = phish.unpack()
   tri = [vi,vj,vk]
   tri.sort()
-  print >>fp,tri[0],tri[1],tri[2]
-
+  if (tri[0],tri[1],tri[2]) not in hash:
+    print >>fp,tri[0],tri[1],tri[2]
+    hash[(tri[0],tri[1],tri[2])] = 0
+    n += 1
+    
 def done():
-  print "Triangle count",n
+  print "Triangle count %d out of %d" % (n,ndup)
   
 args = phish.init(sys.argv)
 phish.input(0,count,done,1)
@@ -25,7 +28,9 @@ for index,argument in enumerate(args):
   if argument == "-f":
     fp = open(args[index+1],"w")
 
-n = 0
+n = ndup = 0
+hash = {}
+
 phish.loop()
 phish.exit()
 
--- minnow/triangle.py
+++ minnow/triangle.py
@@ -4,108 +4,90 @@ import phish
 # process an edge = (Vi,Vj)
 # sent by edge source to owner of Vi
 # ignore self edges
-# if I don't own Vi, store edge
-# else send as (Vj,Vi,Di) with Di = degree of Vi, to owner of Vj on port 1
+# store edge and compute Di = degree of Vi
+# send (Vj,Vi,Di) to owner of Vj on port 1
 
 def edge(nvalues):
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
   if vi == vj: return
-  if vi not in hash:
-    hash[vi] = [vj]
-  else:
-    di = len(hash[vi])
-    phish.pack_uint64(vj)
-    phish.pack_uint64(vi)
-    phish.pack_int32(di)
-    phish.send_key(1,vj)
+  if vi not in hash: hash[vi] = [vj]
+  else: hash[vi].append(vj)
+  di = len(hash[vi])
+  phish.pack_uint64(vj)
+  phish.pack_uint64(vi)
+  phish.pack_int32(di)
+  phish.send_key(1,vj)
 
 def edge_close():
   phish.close(1)
   
 # process an edge + degree = (Vi,Vj,Dj)
 # sent by another triangle minnow in edge() to owner of Vi
-# if I don't own Vi, store edge
-# else if my Di > Dj, send (Vj,Vi) back to owner of Vj on port 2
-# else store edge and emit wedges
-#   wedge count = N*(N-1) for N edges of Vi
-#   wedge = (Vi,Vj,Vk) where Vj,Vk are edges of Vi
-#   send each wedge to owners of 2 other vertices on port 3
+# store edge and compute Di = degree of Vi
+# if Di < Dj, send (Vj,Vi) to owner of Vj on port 2
+# else, send (Vj,Vi,Ni) to owner of Vj on port 3
+#   Ni = neighbor list of Vi
 
 def edge_degree(nvalues):
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
   type,dj,tmp = phish.unpack()
-  if vi not in hash:
-    hash[vi] = [vj]
-  elif len(hash[vi]) > dj:
+  if vi not in hash: hash[vi] = [vj]
+  else: hash[vi].append(vj)
+  di = len(hash[vi])
+  if di < dj:
     phish.pack_uint64(vj)
     phish.pack_uint64(vi)
     phish.send_key(2,vj)
   else:
-    hash[vi].append(vj)
-    list = hash[vi]
-    for j in xrange(len(list)):
-      for k in xrange(j):
-        phish.pack_uint64(vi)
-        phish.pack_uint64(list[j])
-        phish.pack_uint64(list[k])
-        phish.send_key(3,list[j])
-        phish.pack_uint64(vi)
-        phish.pack_uint64(list[k])
-        phish.pack_uint64(list[j])
-        phish.send_key(3,list[k])
+    phish.pack_uint64(vj)
+    phish.pack_uint64(vi)
+    phish.pack_uint64_array(hash[vi])
+    phish.send_key(3,vj)
 
 def edge_degree_close():
   phish.close(2)
 
 # process an edge = (Vi,Vj)
 # sent by another triangle minnow in edge_degree() to owner of Vi
-# store edge and emit wedges
-#   wedge count = N*(N-1) for N edges of Vi
-#   wedge = (Vi,Vj,Vk) where Vj,Vk are edges of Vi
-#   send each wedge to owners of 2 other vertices on port 3
+# send (Vj,Vi,Ni) to owner of Vj on port 3
 
-def edge_wedge(nvalues):
+def edge_request(nvalues):
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
-  hash[vi].append(vj)
-  list = hash[vi]
-  for j in xrange(len(list)):
-    for k in xrange(j):
-      phish.pack_uint64(vi)
-      phish.pack_uint64(list[j])
-      phish.pack_uint64(list[k])
-      phish.send_key(3,list[j])
-      phish.pack_uint64(vi)
-      phish.pack_uint64(list[k])
-      phish.pack_uint64(list[j])
-      phish.send_key(3,list[k])
+  phish.pack_uint64(vj)
+  phish.pack_uint64(vi)
+  phish.pack_uint64_array(hash[vi])
+  phish.send_key(3,vj)
 
-def edge_wedge_close():
+def edge_request_close():
   phish.close(3)
 
-# process a wedge = (Vi,Vj,Vk) where Vi is apex of wedge
-# sent by another triangle minnow in edge_degree() or edge_wedge()
-#   to owner of Vj (and to owner of Vk)
-# if I own edge (Vj,Vk), emit triangle to port 0
+# process a neighbor list = (Vi,Vj,Nj) where Nj = neighbor list of Vj
+# Nj is smaller than Ni, so loop over Nj
+# if value in Nj is in Ni, emit found triangle to port 0
 
-def wedge(nvalues):
+def neigh(nvalues):
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
-  type,vk,tmp = phish.unpack()
-  if vj not in hash: return
-  if vk not in hash[vj]: return
-  phish.repack()
-  phish.send(0)
+  type,nj,tmp = phish.unpack()
+  ni = hash[vi]
+  #print "NEIGH",vi,vj,ni,nj
+  for vk in nj:
+    if vk in ni:
+      phish.pack_uint64(vi)
+      phish.pack_uint64(vj)
+      phish.pack_uint64(vk)
+      phish.send(0)
 
 # main program
   
 args = phish.init(sys.argv)
 phish.input(0,edge,edge_close,1)
 phish.input(1,edge_degree,edge_degree_close,1)
-phish.input(2,edge_wedge,edge_wedge_close,1)
-phish.input(3,wedge,None,1)
+phish.input(2,edge_request,edge_request_close,1)
+phish.input(3,neigh,None,1)
 phish.output(0)
 phish.output(1)
 phish.output(2)
@@ -115,8 +97,6 @@ phish.check()
 if len(args) != 1: phish.error("Triangle syntax: triangle")
 
 hash = {}
-
-me = phish.query("idlocal",0,0)
 
 phish.loop()
 phish.exit()
