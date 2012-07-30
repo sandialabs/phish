d5e4ed4cf618a81683f0ce0efcc2ed7485a261cb
Steve Plimpton <sjplimp@sandia.gov>
2012-07-30 15:45:59 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@353 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- minnow/triangle.py
+++ minnow/triangle.py
@@ -27,8 +27,8 @@ def edge_close():
 # sent by another triangle minnow in edge() to owner of Vi
 # store edge unless duplicate
 # compute Di = degree of Vi
-# if Di < Dj, send (Vj,Vi) to owner of Vj on port 2
-# else, send (Vj,Vi,Ni) to owner of Vj on port 3
+# if Di >= Dj, send (Vj,Vi) to owner of Vj on port 2 as neighbor request
+# else Di < Dj, so send (Vj,Vi,Ni) to owner of Vj on port 3
 #   Ni = neighbor list of Vi
 
 def edge_degree(nvalues):
@@ -38,7 +38,7 @@ def edge_degree(nvalues):
   if vi not in hash: hash[vi] = [vj]
   elif vj not in hash[vi]: hash[vi].append(vj)
   di = len(hash[vi])
-  if di < dj:
+  if di >= dj:
     phish.pack_uint64(vj)
     phish.pack_uint64(vi)
     phish.send_key(2,vj)
@@ -51,11 +51,11 @@ def edge_degree(nvalues):
 def edge_degree_close():
   phish.close(2)
 
-# process an edge = (Vi,Vj)
+# process a neighbor request = (Vi,Vj)
 # sent by another triangle minnow in edge_degree() to owner of Vi
 # send (Vj,Vi,Ni) to owner of Vj on port 3
 
-def edge_request(nvalues):
+def neigh_request(nvalues):
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
   phish.pack_uint64(vj)
@@ -63,14 +63,14 @@ def edge_request(nvalues):
   phish.pack_uint64_array(hash[vi])
   phish.send_key(3,vj)
 
-def edge_request_close():
+def neigh_request_close():
   phish.close(3)
 
 # process a neighbor list = (Vi,Vj,Nj) where Nj = neighbor list of Vj
 # Nj is smaller than Ni, so loop over Nj
 # if value in Nj is in Ni, emit found triangle to port 0
 
-def neigh(nvalues):
+def neighbors(nvalues):
   type,vi,tmp = phish.unpack()
   type,vj,tmp = phish.unpack()
   type,nj,tmp = phish.unpack()
@@ -88,8 +88,8 @@ def neigh(nvalues):
 args = phish.init(sys.argv)
 phish.input(0,edge,edge_close,1)
 phish.input(1,edge_degree,edge_degree_close,1)
-phish.input(2,edge_request,edge_request_close,1)
-phish.input(3,neigh,None,1)
+phish.input(2,neigh_request,neigh_request_close,1)
+phish.input(3,neighbors,None,1)
 phish.output(0)
 phish.output(1)
 phish.output(2)
