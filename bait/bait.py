6232f38f45f898f9e0a54fa33c76f034989d06cb
Steve Plimpton <sjplimp@sandia.gov>
2012-08-16 21:53:49 +0000

    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@371 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- bait/bait.py
+++ bait/bait.py
@@ -1,10 +1,7 @@
 # Bait.py pre-processing script
 
 import phish.bait
-import optparse
-import os
-import re
-import sys
+import optparse,os,re,sys,string
 
 def variable_callback(option,opt_str,value,parser):
   def floatable(string):
@@ -28,6 +25,29 @@ def variable_callback(option,opt_str,value,parser):
   value = (value[0], value[1:])
   parser.values.variable.append(value)
 
+def starparse(str,n,param):
+  star = str.find('*')
+  if star < 0: start = stop = int(str)
+  elif star == 0 and len(str) == 1:
+    start = 0
+    stop = n-1
+  elif star == 0:
+    start = 0
+    stop = int(str[1:])
+  elif star == len(str)-1:
+    start = int(str[:star])
+    stop = n-1
+  else:
+    start = int(str[:star])
+    stop = int(str[star+1:])
+  if start < 0 or start >= n:
+    raise Exception("Invalid school bind parameter %s" % param)
+  if stop < 0 or stop >= n:
+    raise Exception("Invalid school bind parameter %s" % param)
+  if start > stop:
+    raise Exception("Invalid school bind parameter %s" % param)
+  return start,stop
+  
 # main program
 # parse command-line options
 
@@ -55,9 +75,7 @@ options, arguments = parser.parse_args()
 paths = options.path.split(":")
 settings = dict(options.set)
 variables = dict([(key, value) for key,value in options.variable])
-
 if options.verbose: settings["verbose"] = "true"
-
 schools = {}
 hooks = []
 
@@ -100,11 +118,13 @@ for line_number, line in enumerate(script):
   # handle specific commands
 
   if command == "set":
+    if len(arguments) != 2: raise Exception("Invalid set command")
     key = arguments[0]
     value = arguments[1]
     settings[key] = value
 
   elif command == "variable":
+    if len(arguments) == 1: raise Exception("Invalid variable command")
     key = arguments[0]
     values = arguments[1:]
     if key not in variables:
@@ -113,30 +133,93 @@ for line_number, line in enumerate(script):
   elif command == "minnow":
     id = arguments[0]
     arguments = arguments[1:]
-    if id in schools:
-      raise Exception("Minnow cannot use duplicate id '%s'" % id)
+    if id in schools: raise Exception("Duplicate ID in minnow command")
     schools[id] = {"index" : len(schools), "arguments" : arguments,
-                   "count" : 1, "host" : ""}
+                   "count" : 1, "host" : "", "bind" : []}
 
   elif command == "hook":
+    if len(arguments) != 3: raise Exception("Invalid hook command")
     output = arguments[0].split(":")
     style = arguments[1]
     input = arguments[2].split(":")
     hooks.append((output[0],int(output[1]) if len(output) > 1 else 0,
                   style, int(input[1]) if len(input) > 1 else 0, input[0]))
 
+  # school has host and bind options
+    
   elif command == "school":
+    if len(arguments) < 2: raise Exception("Invalid school command")
     id = arguments[0]
     count = arguments[1]
     keywords = arguments[2:]
+    if id not in schools: raise Exception("Unknown minnow ID in school command")
     schools[id]["count"] = int(count)
-    for key,value in keywords:
-      if key == "host": schools[id]["host"] = value
-
+    iarg = 0
+    while iarg < len(keywords):
+      if keywords[iarg] == "host":
+        if iarg+2 > len(keywords):
+          raise Exception("Invalid syntax in school command")
+        schools[id]["host"] = keywords[iarg+1]
+        iarg += 2
+      elif keywords[iarg] == "bind":
+        if iarg+2 > len(keywords):
+          raise Exception("Invalid syntax in school command")
+        iargstart = iarg+1
+        iarg = iargstart
+        while iarg < len(keywords):
+          if keywords[iarg][0] != '*' and \
+                keywords[iarg][0] not in string.digits: break
+          iarg += 1
+        schools[id]["bind"] = keywords[iargstart:iarg]
+          
   else:
     raise Exception("Unknown command '%s' on line %s: %s" %
                     (command, line_number, line))
 
+if len(schools) == 0: raise Exception("No minnows defined")
+  
+# convert bind params to list of [node,core] for each minnow instance
+# expand each bind param with wildcards into nclist = node,core list
+# assign count instances of minnow to leading entries in nclist
+# if any school has bind params, all of them must
+
+if "pernode" in settings: pernode = int(settings["pernode"])
+else: pernode = 1
+if pernode == 0: raise Exception("Invalid pernode setting")
+
+nprocs = 0
+for id,school in schools.items(): nprocs += school["count"]
+numnodes = nprocs / pernode
+if nprocs % pernode: numnodes += 1
+
+bindflag = -1
+for id,school in schools.items():
+  bindparams = school["bind"]
+  if bindparams and bindflag < 0: bindflag = 1
+  if not bindparams and bindflag < 0: bindflag = 0
+  if bindparams and bindflag == 0:
+    raise Exception("All or no school commands must use bind option")
+  if not bindparams and bindflag:
+    raise Exception("All or no school commands must use bind option")
+  if not bindparams: continue
+  nclist = []
+  for param in bindparams:
+    comma = param.find(',')
+    if comma < 0: raise Exception("Invalid syntax in school command")
+    nodes = param[:comma]
+    cores = param[comma+1:]
+    nodestart,nodestop = starparse(nodes,numnodes,param)
+    corestart,corestop = starparse(cores,pernode,param)
+  for inode in range(nodestart,nodestop+1):
+    for icore in range(corestart,corestop+1):
+      nclist.append([inode,icore])
+  if len(nclist) < school["count"]:
+    raise Exception("Insufficient bind params for school count")
+  bindlist = []
+  for i in range(school["count"]):
+    bindlist.append(nclist[i])
+  school["bind"] = bindlist
+  
 # add suffixes to school executables
 # assumes executable is 1st arg
 
@@ -167,7 +250,7 @@ if options.verbose:
     sys.stderr.write("BAIT Setting: %s %s\n" % (name, value))
 
 # pass parsed data to the Bait.py backend
-# output schools in minnow order
+# output the schools in minnow order
 
 phish.bait.backend(options.backend)
 
@@ -175,7 +258,8 @@ for name,value in settings.items():
   phish.bait.set(name,value)
 
 for id,school in sorted(schools.items(), key=lambda x: x[1]["index"]):
-  phish.bait.school(id,[school["host"]] * school["count"],school["arguments"])
+  phish.bait.school(id,school["count"]*[school["host"]],
+                    school["bind"],school["arguments"])
 
 for output_id,output_port,style,input_port,input_id in hooks:
   phish.bait.hook(output_id,output_port,style,input_port,input_id)
