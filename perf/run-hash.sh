e9b769fe7891a3198c1e0d95e1e4bac3feb6a15f
Tim Shead <tshead@sandia.gov>
2012-09-14 21:17:16 +0000

    Remove the perf directory, which is obsolete.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@537 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- perf/run-hash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python run-cpp-phish-zmq-hashed.py --processes 2/256 > cpp-phish-zmq-hashed-tcp.csv
-python run-cpp-phish-mpi-hashed.py --processes 2/256 > cpp-phish-mpi-hashed-fastest.csv
-##python run-cpp-phish-mpi-hashed.py --mpi-extra="--mca btl self,tcp" > cpp-phish-mpi-hashed-tcp.csv
-
