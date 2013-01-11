bb041b6d54df5a1bbb3903a4000dc3ec95bf756c
Tim Shead <tshead@sandia.gov>
2013-01-11 21:54:10 +0000

    Send 10 million messages in the ring test, to match Jon's use case.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@583 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- minnow/ring-source.py
+++ minnow/ring-source.py
@@ -8,7 +8,7 @@ sys.argv = phish.init(sys.argv)
 
 parser = optparse.OptionParser()
 parser.add_option("--receiver", type="int", default=0, help="Message recipient.  Default: %default.")
-parser.add_option("--message-count", "-n", type="int", default=1000000, help="Message count.  Default: %default.")
+parser.add_option("--message-count", "-n", type="int", default=10000000, help="Message count.  Default: %default.")
 parser.add_option("--message-size", "-m", type="int", default=80, help="Message size.  Default: %default.")
 options, arguments = parser.parse_args()
 
