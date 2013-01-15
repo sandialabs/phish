5aef7794d31422eb0b849ba829b87ff58069fc64
Tim Shead <tshead@sandia.gov>
2013-01-15 16:08:23 +0000

    Add an optional delay to the ring benchmark source minnow.
    git-svn-id: svn+ssh://software-srn.sandia.gov/svn/phish/trunk@589 76edfe0a-4bdc-4aa6-b265-a765f6f55952

--- minnow/ring-source.py
+++ minnow/ring-source.py
@@ -3,13 +3,15 @@
 import optparse
 import phish
 import sys
+import time
 
 sys.argv = phish.init(sys.argv)
 
 parser = optparse.OptionParser()
 parser.add_option("--receiver", type="int", default=0, help="Message recipient.  Default: %default.")
-parser.add_option("--message-count", "-n", type="int", default=10000000, help="Message count.  Default: %default.")
+parser.add_option("--message-count", "-n", type="int", default=1000000, help="Message count.  Default: %default.")
 parser.add_option("--message-size", "-m", type="int", default=80, help="Message size.  Default: %default.")
+parser.add_option("--delay", type="float", default=0, help="Delay in seconds between messages.  Default: %default.")
 options, arguments = parser.parse_args()
 
 phish.output(0)
@@ -19,6 +21,7 @@ message = "*" * options.message_size
 for n in range(options.message_count):
   phish.pack_string(message)
   phish.send_direct(0, options.receiver)
+  time.sleep(options.delay)
 
 phish.pack_string("stop")
 phish.send_direct(0, options.receiver)
